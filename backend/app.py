19b95941490be8f2bfcb41e97888f8117e78bf5a
chieffy99 <155105479+chieffy99@users.noreply.github.com>
2025-06-12 03:37:09 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,19 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-import logic_engine.interpreter as interp
+ from fastapi import FastAPI
+ from pydantic import BaseModel
+ import logic_engine.interpreter as interp
+ 
+ app = FastAPI(title="DDW Anti-Normalization API")
+ 
 
-app = FastAPI(title="DDW Anti-Normalization API")
+ @app.get("/")
+ def read_root():
+     return {"msg": "DDW Anti-Normalization API"}
+ 
 
-@app.get("/")
-def read_root():
-    return {"msg": "DDW Anti-Normalization API"}
+ class Logic(BaseModel):
+     record: str
+ 
 
-class Logic(BaseModel):
-    record: str
-
-@app.post("/interpret")
-def interpret(item: Logic):
-    return interp.interpret(item.record)
+ @app.post("/interpret")
+ def interpret(item: Logic):
+     return interp.interpret(item.record)
