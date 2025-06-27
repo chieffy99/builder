c1d091fb4f32c283b101230dcb28d9a537c8efe0
chieffy99 <155105479+chieffy99@users.noreply.github.com>
2025-06-27 13:27:28 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
++#!/usr/bin/env bash
++# 1. Install main packages
++pip install -r requirements.txt
++
++# 2. Install linter and type checker
++pip install flake8 black pyright
++
++# 3. (Optional) Persist any exports
++echo 'export PYTHONPATH="$PWD"' >> ~/.bashrc
