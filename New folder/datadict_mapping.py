972cce53c3a1f42c9641b04b7342f0e38323349e
chieffy99 <155105479+chieffy99@users.noreply.github.com>
2025-07-02 23:39:03 +0700

    [setup] add requirements and fix lint

--- New folder/datadict_mapping.py
+++ New folder/datadict_mapping.py
@@ -1,20 +1,24 @@
 import json
 
-# โหลด dictionary
 with open("dictionary.json", "r", encoding="utf-8") as f:
     datadict = json.load(f)
+
 
 def map_statN(code):
     return datadict["statN"].get(str(code), "Unknown")
 
+
 def map_statC(code):
     return datadict["statC"].get(str(code), "Unknown")
+
 
 def map_slot(slot):
     return datadict["slotMeaning"].get(slot, "Unknown")
 
+
 def map_ID1(code):
     return datadict["ID1"].get(code, "Unknown")
 
+
 def map_ID2(code):
-    return datadict["ID2"].get(code,
+    return datadict["ID2"].get(code, "Unknown")
